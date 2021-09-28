0fb1ff146e7674937a4a7411e4048f393c62f92d
Vamsi Krishna Kanagala <vkanaga1@asu.edu>
2021-09-28 04:55:01 -0700

    Changes

--- projects/Project-1/graph.py
+++ projects/Project-1/graph.py
@@ -5,18 +5,17 @@ Created on Mon Sep 27 19:18:45 2021
 @author: Vamsi
 """
 
-import json
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 import nltk
-import itertools
-import networkx as nx
-import string
-import re
 from nltk import bigrams
 from nltk.corpus import  stopwords as sw
 from nltk.tokenize import word_tokenize
+
+import itertools
+import networkx
+import re
 from collections import Counter
 
 nltk.download('stopwords')
@@ -31,45 +30,34 @@ data = media[['id', 'title' ,'description']]
 
 data = data.dropna()
 
-data['title'] = data['title'].apply( lambda text: re.sub('[^A-Za-z0-9 ]+', '', text))
+data['title']       = data['title'].apply( lambda text: re.sub('[^A-Za-z0-9 ]+', '', text))
 data['description'] = data['description'].apply( lambda text: re.sub('[^A-Za-z0-9 ]+', '', text))
-data['title'] = data['title'].apply( lambda text: ' '.join([word for word in word_tokenize(text) if not word in stopwords and len(word)>2 ]))
+data['title']       = data['title'].apply( lambda text: ' '.join([word for word in word_tokenize(text) if not word in stopwords and len(word)>2 ]))
 data['description'] = data['description'].apply( lambda text: ' '.join([word for word in word_tokenize(text) if not word in stopwords and len(word)>2 ]))
-data['title'] = data['title'].apply(lambda x: x.upper())
+data['title']       = data['title'].apply(lambda x: x.upper())
 data['description'] = data['description'].apply(lambda x: x.upper())
 
-title_bigram = []
-for title in data['title']:
-    title_bigram.append((list(bigrams(title.split()))))
+network_title = []
+for title in data['title']:  network_title.append((list(bigrams(title.split()))))
 
-bigrams = list(itertools.chain(*title_bigram))
-bigram_counts = Counter(bigrams)
-bigram_df = pd.DataFrame(bigram_counts.most_common(250), columns = ['bigram','count'])
+bigram_graph = list(itertools.chain(*network_title))
+bigram_graph_counts = Counter(bigram_graph)
+bigram_graph_df = pd.DataFrame(bigram_graph_counts.most_common(250), columns = ['bigram','count'])
 
-d = bigram_df.set_index('bigram').T.to_dict('records')
-G = nx.Graph()
+d = bigram_graph_df.set_index('bigram').T.to_dict('records')
+graph = networkx.Graph()
 
-for k, v in d[0].items():
-    G.add_edge(k[0], k[1], weight=(v * 10))
+for k, v in d[0].items():  graph.add_edge(k[0], k[1], weight=(v * 15))
 
-fig, ax = plt.subplots(figsize=(50, 40))
+fig, axis = plt.subplots(figsize=(100, 80))
 
-pos = nx.spring_layout(G, k=2)
+position = networkx.spring_layout(graph, k=3)
 
-nx.draw_networkx(G, pos,
-                 font_size=16,
-                 width=3,
-                 edge_color='black',
-                 node_color='blue',
-                 with_labels = False,
-                 ax=ax)
+networkx.draw_networkx(graph, position, font_size=30, width=6, edge_color='grey', node_color='green', with_labels = False, ax = axis)
 
-for key, value in pos.items():
-    x, y = value[0]+.13, value[1]+.03
-    ax.text(x, y,
-            s=key,
-            bbox=dict(facecolor='red', alpha=0.25),
-            horizontalalignment='center', fontsize=13)
+for key, value in position.items():
+    x, y = value[0], value[1]
+    axis.text(x, y, s=key, bbox=dict(facecolor='yellow', alpha = 0.25), horizontalalignment='center', fontsize = 30)
     
 plt.show()
 
@@ -82,15 +70,14 @@ def drawHistogram(dict,title,xlabel,ylabel):
     plt.xticks(rotation=90)
     plt.show()
     
-    
-pos = nx.spring_layout(G,scale=10,k=0.25)
-nx.draw(G,pos,with_labels=True,node_size=1200,node_shape='s',alpha=0.5,font_size=16)
-plt.show() #Shows the window containing the graph
-degreeCentrality=nx.algorithms.centrality.degree_centrality(G)
-betweeness=nx.algorithms.centrality.betweenness_centrality(G)
-closeness=nx.algorithms.centrality.closeness_centrality(G)
-df_measures=pd.DataFrame([degreeCentrality,betweeness,closeness])
-df_measures=df_measures.rename(index={0:'DegreeCentrality',1:'Betweenness',2:'Closeness'}).transpose()
-drawHistogram(degreeCentrality,"Degree Centrality Histogram","Screen Name","Degree Centrality")
+
+degreeCentrality   = networkx.algorithms.centrality.degree_centrality(graph)
+betweeness         = networkx.algorithms.centrality.betweenness_centrality(graph)
+closeness          = networkx.algorithms.centrality.closeness_centrality(graph)
+
+measures = pd.DataFrame([degreeCentrality,betweeness,closeness])
+measures = measures.rename(index={0:'DegreeCentrality',1:'Betweenness',2:'Closeness'}).transpose()
+
+drawHistogram(degreeCentrality,"Degree Centrality Histogram","Screen Name", "Degree Centrality")
 drawHistogram(betweeness, "Betweeness Histogram", "Screen Name","Betweeness")
 drawHistogram(closeness, "Closeness Histogram", "Screen Name","Closeness")
